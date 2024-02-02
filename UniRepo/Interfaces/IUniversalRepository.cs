dc277f13ac574cd2cc3309664673e25b7109c1f9
Daulet Kshibekov <Daulet_Kshibekov@epam.com>
2024-02-02 20:09:16 +0600

    Add project files.

---
+++ UniRepo/Interfaces/IUniversalRepository.cs
@@ -0,0 +1,46 @@
+﻿namespace UniRepo.Interfaces;
+
+public interface IUniversalRepository<TEntity, in TIdType>
+{
+    /// <summary>
+    /// Asynchronously retrieves an entity of type <typeparamref name="TEntity"/> based on a composite primary key.
+    /// </summary>
+    /// <param name="id">The identifier of the entity to retrieve. The type of the identifier is <typeparamref name="TIdType"/>.</param>
+    /// <param name="isReadonly">A boolean value indicating whether the entity should be retrieved in a read-only mode (AsNoTracking). 
+    /// If set to true, the entity is retrieved without tracking changes (more efficient for read-only scenarios). 
+    /// If false, the entity is tracked by the DbContext, allowing for subsequent updates.
+    /// The parameter is optional. Default value is false.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the entity matching the provided identifier.
+    /// If no entity is found, the task result is null.
+    /// </returns>
+    /// <remarks>
+    /// This method offers the flexibility to fetch entities in either a tracked or non-tracked state. 
+    /// Tracked entities are suitable for scenarios where updates to the entity are expected, as changes are tracked by the DbContext and can be persisted back to the database.
+    /// Non-tracked entities are more efficient for read-only operations, as they do not incur the overhead of change tracking.
+    /// </remarks>
+    Task<TEntity?> GetByIdAsync(TIdType id, bool isReadonly = false);
+
+    /// <summary>
+    /// Asynchronously retrieves an entity of type <typeparamref name="TEntity"/> based on a composite primary key.
+    /// Starts tracking found entity.
+    /// </summary>
+    /// <param name="keys">An IEnumerable collection of keys representing the composite primary key of the entity.
+    /// The number and order of keys should match the entity's primary key definition.</param>
+    /// <param name="isReadonly">A boolean value indicating whether the entity should be retrieved in a read-only mode (AsNoTracking). 
+    /// If set to true, the entity is retrieved without tracking changes (more efficient for read-only scenarios). 
+    /// If false, the entity is tracked by the DbContext, allowing for subsequent updates.
+    /// The parameter is optional. Default value is false.</param>
+    /// <remarks>
+    /// Constructs a dynamic query based on the provided keys and the primary key properties of the <typeparamref name="TEntity"/>.
+    /// Throws an <see cref="InvalidOperationException"/> if the primary key for the entity is not defined or if no key properties are found.
+    /// An <see cref="ArgumentException"/> is thrown if the number of provided keys does not match the number of keys for the entity.
+    /// </remarks>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains the entity matching the provided keys.
+    /// If no entity is found, or if the keys do not match, the task result is null.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">Thrown when the primary key definition for the entity is not found or if no key properties are found.</exception>
+    /// <exception cref="ArgumentException">Thrown when the number of provided keys does not match the number of keys for the entity.</exception>
+    Task<TEntity?> GetByIdAsync(IEnumerable<TIdType> keys, bool isReadonly = false);
+}
---
+++ UniRepo/Repositories/UniversalRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Linq.Expressions;
+using UniRepo.Interfaces;
+
+namespace UniRepo.Repositories;
+
+public class UniversalRepository<TEntity, TIdType> : IUniversalRepository<TEntity, TIdType>
+    where TEntity : class, IUniRepoEntity<TIdType>
+{
+    private readonly DbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public UniversalRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    /// <inheritdoc />
+    public async Task<TEntity?> GetByIdAsync(TIdType id, bool isReadonly = false)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+
+        return isReadonly
+            ? await _dbSet.AsNoTracking().FirstOrDefaultAsync(e => e.Id != null && e.Id.Equals(id))
+            : await _dbSet.FindAsync(id);
+    }
+
+
+    /// <inheritdoc />
+    public async Task<TEntity?> GetByIdAsync(IEnumerable<TIdType> keys, bool isReadonly = false)
+    {
+        ArgumentNullException.ThrowIfNull(keys);
+
+        var keysArray = keys.ToArray();
+
+        if (_context.Model.FindEntityType(typeof(TEntity))?.FindPrimaryKey() is not { } compositeKey)
+            throw new InvalidOperationException($"Primary key for entity of type {typeof(TEntity).Name} not found.");
+
+        var keyProperties = compositeKey.Properties;
+
+        if (keysArray.Length != keyProperties.Count)
+            throw new ArgumentException($"The number of provided keys does not match the number of keys for entity of type {typeof(TEntity).Name}.");
+
+        var (parameter, predicate) = GetQueryExpression(keysArray, keyProperties);
+
+        var lambda = Expression.Lambda<Func<TEntity, bool>>(predicate, parameter);
+
+        return isReadonly
+            ? await _dbSet.AsNoTracking().Where(lambda).FirstOrDefaultAsync()
+            : await _dbSet.Where(lambda).FirstOrDefaultAsync();
+    }
+
+    #region --------------------------- Private methods ---------------------------
+
+    private static (ParameterExpression, Expression) GetQueryExpression(TIdType[] keys, IReadOnlyList<IProperty> keyProperties)
+    {
+        var parameter = Expression.Parameter(typeof(TEntity), "e");
+        Expression? predicate = null;
+
+        for (var i = 0; i < keyProperties.Count; i++)
+        {
+            var keyProperty = keyProperties[i];
+            var keyName = keyProperty.Name;
+            var keyVal = Expression.Constant(keys[i], typeof(TIdType));
+
+            var property = Expression.Property(parameter, keyName);
+            var equals = Expression.Equal(property, keyVal);
+
+            predicate = predicate == null ? equals : Expression.AndAlso(predicate, equals)
+                ?? throw new InvalidOperationException("No key properties found.");
+        }
+
+        return (parameter, predicate!);
+    }
+
+    #endregion #region --------------------------- Private methods ---------------------------
+}
